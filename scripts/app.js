3e160824310c5f136824a1200d58a134e585b104
d-dilbaghsingh <dilbaghsingh5016@gmail.com>
2024-06-12 20:28:46 -0400

    Add files via upload

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+const myFullName = "Dilbagh Singh";
+const myStudentNumber = "1163754";
+console.log($,{myFullName} - $,{myStudentNumber});
+const headerContent = document.querySelector('h1');
+headerContent.innerHTML = $,{myFullName} - $,{myStudentNumber};
+headerContent.classList.add('headingPrimary');
